283b263d969c13d679a9095fea4acdd9d82141d9
suraj bangale <sgbangale@gmail.com>
2018-06-27 00:19:54 +0530

    added formgroup and array for schema

--- src/app/shared/entity/entity.component.ts
+++ src/app/shared/entity/entity.component.ts
@@ -9,7 +9,7 @@ import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
 })
 export class EntityComponent {
 
-  schemaColumns :any =[];
+  fieldTypes :any =[];
   selectedItems: any[];
   formGroup: FormGroup;
   entity_schema_group : FormGroup;
@@ -20,45 +20,41 @@ export class EntityComponent {
   isNewField : boolean;
   constructor(private fb :FormBuilder) {
     this.createForm() ;
-    this.schemaColumns = 
+    this.fieldTypes = 
     [
-      {field : "code" ,header:"Field Code"},
-      {field : "type" ,header:"Field Type"},
-      {field : "required" ,header:"Is Required"}
+      {label:'String', value:"String"},
+      {label : "Number" ,value:"Number"},
+      {label : "Mixed" ,value:"Mixed"}
     ]
-
-
-    
   }
   save()
   {
     this.formGroup.value.entity_schema_group = this.formGroup.get('entity_schema_group') as FormArray;
     this.formGroup.value.entity_schema_group.push(this.createFieldItem());
   }
-  showDialogToAdd() {
-    this.isNewField = true;    
-    this.displayDialog = true;
+  deleteField(index)
+  {
+    //(this.formGroup.get('entity_fields') as FormArray).splice(index,0);
+    console.log(index);
+  }
+  addField() {
+  (this.formGroup.get('entity_fields') as FormArray).push(this.createFieldItem());
 }
-createFieldItem()
+createFieldItem() :FormGroup
 {
   return this.fb.group({
     field_code:['',[Validators.required]],
     field_type:['',[Validators.required]],
-    field_required:['',[Validators.required]]
+    field_required:[true,[Validators.required]]
   });
 }
   createForm() {
-
-    this.entity_schema_group=
-
-    //var schemaGroup =this.fb.array([schema]);
-
     this.formGroup = this.fb.group({
       entity_code: ['', [Validators.required]],
       entity_name: ['', [Validators.required]],
       entity_access:['', [Validators.required]],
       entity_active:['', [Validators.required]],
-      entity_schema_group :this.fb.array([])
+      entity_fields :this.fb.array([this.createFieldItem()])
     });
   }
 
